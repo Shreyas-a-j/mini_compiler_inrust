1e8a4b9901c4bf341d841ca2810971a533aafd9a
Shreyas Jadhav <airjadhav111@gmail.com>
2026-09-06 12:36:46 +0530

    simple impl of a lexer to identify tokens

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,44 @@ enum Token {
     EOF,
 }
 
-fn main() {
-    let token = Token::Let;
-
-    println!("{:?}", token);
+struct Lexer {
+    input: Vec<char>,
+    position: usize,
 }
+
+impl Lexer {
+    fn new(input: &str) -> Self {
+        Self {
+            input: input.chars().collect(),
+            position: 0,
+        }
+    }
+
+    fn next_token(&mut self) -> Token {
+        if self.position >= self.input.len() {
+            return Token::EOF;
+        }
+
+        let ch = self.input[self.position];
+
+        self.position += 1;
+
+        match ch {
+            '=' => Token::Equal,
+            '+' => Token::Plus,
+            ';' => Token::Semicolon,
+            _ => Token::EOF,
+        }
+    }
+}
+
+fn main() {
+    let mut lexer = Lexer::new("+=;");
+
+    println!("{:?}", lexer.next_token());
+    println!("{:?}", lexer.next_token());
+    println!("{:?}", lexer.next_token());
+    println!("{:?}", lexer.next_token());
+
+}
+
