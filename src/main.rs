79b382d9bc24229f772bc85117610365c1ce7f24
Shreyas Jadhav <airjadhav111@gmail.com>
2026-09-06 13:28:08 +0530

    number handled in lexer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #[derive(Debug, PartialEq)]
+
 enum Token {
     Let,
+    If,
+    Else,
+    Return,
+
     Identifier(String),
     Integer(i64),
 
@@ -25,30 +30,109 @@ impl Lexer {
     }
 
     fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
         if self.position >= self.input.len() {
             return Token::EOF;
         }
+        
 
         let ch = self.input[self.position];
 
-        self.position += 1;
-
         match ch {
-            '=' => Token::Equal,
-            '+' => Token::Plus,
-            ';' => Token::Semicolon,
-            _ => Token::EOF,
+            '=' => {
+                self.position += 1;
+                Token::Equal
+            }
+
+            '+' => {
+                self.position += 1;
+                Token::Plus
+            }
+
+            ';' => {
+                self.position += 1;
+                Token::Semicolon
+            }
+
+            ch if ch.is_alphabetic() || ch == '_' => {
+                let identifier = self.read_identifier();
+                Self::lookup_keyword(&identifier)
+            }
+
+            ch if ch.is_ascii_digit() => {
+                let number = self.read_number();
+                Token::Integer(number)
+            }
+
+            _ => {
+                self.position += 1;
+                Token::EOF
+            }
         }
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let start = self.position;
+
+        while self.position < self.input.len()
+            && (self.input[self.position].is_alphanumeric()
+                || self.input[self.position] == '_')
+        {
+            self.position += 1;
+        }
+
+        self.input[start..self.position]
+            .iter()
+            .collect()
+    }
+
+    fn lookup_keyword(identifier: &str) -> Token {
+        match identifier {
+            "let" => Token::Let,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "return" => Token::Return,
+            _ => Token::Identifier(identifier.to_string()),
+        }
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.position < self.input.len()
+            && self.input[self.position].is_whitespace()
+        {
+            self.position += 1;
+        }
+    }
+
+    fn read_number(&mut self) -> i64 {
+        let start = self.position;
+
+        while self.position < self.input.len() 
+            && self.input[self.position].is_ascii_digit()
+        {
+            self.position += 1;
+        }
+
+        let number: String = self.input[start..self.position]
+            .iter()
+            .collect();
+
+        number.parse::<i64>().unwrap()
     }
 }
 
 fn main() {
-    let mut lexer = Lexer::new("+=;");
+    let mut lexer = Lexer::new("let x = 10 + 20;");
 
-    println!("{:?}", lexer.next_token());
-    println!("{:?}", lexer.next_token());
-    println!("{:?}", lexer.next_token());
-    println!("{:?}", lexer.next_token());
+    loop {
+        let token = lexer.next_token();
 
+        println!("{:?}", token);
+
+        if token == Token::EOF {
+            break;
+        }
+    }
 }
 
